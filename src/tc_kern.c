c6b41065efc6d832fadd59162d38fd2b4655d008
Hai Huang <huang195@gmail.com>
2022-05-24 15:25:35 -0400

    update tc code to get map working

--- src/tc_kern.c
+++ src/tc_kern.c
@@ -4,26 +4,43 @@
 
 #include "xdp_common.h"
 
-char LICENSE[] SEC("license") = "Dual BSD/GPL";
+#ifndef lock_xadd
+# define lock_xadd(ptr, val)              \
+        ((void)__sync_fetch_and_add(ptr, val))
+#endif
+
+#define PIN_GLOBAL_NS		2
+struct bpf_elf_map {
+	__u32 type;
+	__u32 size_key;
+	__u32 size_value;
+	__u32 max_elem;
+	__u32 flags;
+	__u32 id;
+	__u32 pinning;
+};
 
 #define TC_INGRESS_MAP_INDEX    0
 #define TC_EGRESS_MAP_INDEX     1
 
-struct {
-    __uint(type, BPF_MAP_TYPE_PERCPU_HASH);
-    __uint(max_entries, 2);
-    __type(key, __u32);                   // ingress/egress
-    __type(value, __u32);                 // bytes
-    __uint(pinning, LIBBPF_PIN_BY_NAME);
-} tc_stats_map SEC(".maps") ;
+
+// this map is found at:
+// bpftool map dump pinned /sys/fs/bpf/tc/globals/tc_stats_map
+struct bpf_elf_map SEC("maps") tc_stats_map = {
+	.type = BPF_MAP_TYPE_ARRAY,
+	.size_key = sizeof(__u32),
+	.size_value = sizeof(__u32),
+	.pinning = PIN_GLOBAL_NS,
+	.max_elem = 2,
+};
 
 static __always_inline int tc_stats_record(struct __sk_buff *skb, __u32 index) {
 
     __u32 *bytes;
 
     bytes = bpf_map_lookup_elem(&tc_stats_map, &index);
-    if (bytes) {
-        bytes += skb->len;
+    if (bytes && skb) {
+        lock_xadd(bytes, skb->len);
     }
     return TC_ACT_OK;
 }
