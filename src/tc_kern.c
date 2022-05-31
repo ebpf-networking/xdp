4b79781a997f4f064fd7a00c7d842b22823be858
Hai Huang <huang195@gmail.com>
2022-05-31 09:19:55 -0400

    change to percpu array

--- src/tc_kern.c
+++ src/tc_kern.c
@@ -8,26 +8,12 @@
         ((void)__sync_fetch_and_add(ptr, val))
 #endif
 
-/*
-#define PIN_GLOBAL_NS		2
-struct bpf_elf_map {
-	__u32 type;
-	__u32 size_key;
-	__u32 size_value;
-	__u32 max_elem;
-	__u32 flags;
-	__u32 id;
-	__u32 pinning;
-};
-*/
-
 #define TC_INGRESS_MAP_INDEX    0
 #define TC_EGRESS_MAP_INDEX     1
 
-// this map is found at:
-// bpftool map dump pinned /sys/fs/bpf/tc/globals/tc_stats_map
+// Make sure to use the tc compiled locally to inject this ebpf code
 struct bpf_elf_map SEC("maps") tc_stats_map = {
-	.type = BPF_MAP_TYPE_ARRAY,
+	.type = BPF_MAP_TYPE_PERCPU_ARRAY,
 	.size_key = sizeof(__u32),
 	.size_value = sizeof(__u32),
 	.pinning = PIN_GLOBAL_NS,
@@ -40,7 +26,7 @@ static __always_inline int tc_stats_record(struct __sk_buff *skb, __u32 index) {
 
     bytes = bpf_map_lookup_elem(&tc_stats_map, &index);
     if (bytes && skb) {
-        lock_xadd(bytes, skb->len);
+        bytes += skb->len;
     }
     return TC_ACT_OK;
 }
