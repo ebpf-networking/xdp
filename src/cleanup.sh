100b8e8cbb19180db5ff5e0aa39f2f20032300a5
Hai Huang <huang195@gmail.com>
2022-06-03 14:35:35 -0400

    use locally built tools for cleanup

--- src/cleanup.sh
+++ src/cleanup.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-xdp-loader unload veth-ns1 --all
-xdp-loader unload veth-ns2 --all
-xdp-loader unload veth-ns3 --all
-tc filter del dev veth-ns1 ingress
-tc filter del dev veth-ns1 egress
-tc filter del dev veth-ns2 ingress
-tc filter del dev veth-ns2 egress
-tc filter del dev veth-ns3 ingress
-tc filter del dev veth-ns3 egress
+.output/xdp-tools/xdp-loader unload veth-ns1 --all
+.output/xdp-tools/xdp-loader unload veth-ns2 --all
+.output/xdp-tools/xdp-loader unload veth-ns3 --all
+.output/iproute2/tc qdisc add dev veth-ns1 clsact
+.output/iproute2/tc qdisc add dev veth-ns2 clsact
+.output/iproute2/tc qdisc add dev veth-ns2 clsact
+.output/xdp-tools/tc filter del dev veth-ns1 ingress
+.output/xdp-tools/tc filter del dev veth-ns1 egress
+.output/xdp-tools/tc filter del dev veth-ns2 ingress
+.output/xdp-tools/tc filter del dev veth-ns2 egress
+.output/xdp-tools/tc filter del dev veth-ns3 ingress
+.output/xdp-tools/tc filter del dev veth-ns3 egress
 ip netns del ns1
 ip netns del ns2
 ip netns del ns3
